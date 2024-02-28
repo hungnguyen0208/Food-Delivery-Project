30841cb0932e03fe022fd3a690018569a40f72dd
Hung Nguyen <hungnguyen.k51.ftu@gmail.com>
2024-02-28 22:12:07 +0700

    Initialize Food Delivery project

---
+++ src/main/java/com/maxnguyen/fooddelivery/FooddeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.maxnguyen.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxnguyen/fooddelivery/FooddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxnguyen.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
